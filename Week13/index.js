0ea78437961f6d569d462869568697fd594d0cf6
SevanthAG <sevanthrao1406@gmail.com>
2026-08-26 23:06:15 +0530

    SQL Basic Done

--- Week13/index.js
+++ Week13/index.js
@@ -1,15 +1,54 @@
 const express = require('express')
+const { Pool } = require('pg')
+require('dotenv').config()
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL
+})
 
 const app = express;
 app.use(express.json())
 
-app.post('/signup', (req, res)=> {
+
+
+app.post('/signup',async (req, res)=> {
+    const username = req.body.username
+    const email = req.body.email
+    const password = req.body.password
+
+    const result = await pool.query(
+        `INSERT INTO users(username, email, password)
+        VALUES($1, $2, $3)
+        RETURNING id`,[username, email, password]
+    )
+
+    return res.json({
+        message: "Signup successfull",
+        id: result.rows[0]
+    })
 
 })
 
 
-app.post('/signin', (req, res)=> {
-    
+app.post('/signin',async (req, res)=> {
+    const username = req.body.username
+    const password = req.body.password
+
+    const result = await pool.query(
+        `SELECT * FROM users 
+        WHERE email = '$1' AND password='$2'`
+    )
+    const isExist = result.rows[0]
+
+    if(!isExist){
+        res.status(404).json({
+            message: "Invalid Credential"
+        })
+        return
+    }
+
+    return res.json({
+        message: "Login Successfull",
+    })
 })
 
 app.listen(3000)
